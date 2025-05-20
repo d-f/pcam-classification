aa9f1f6266eb1e603dd7f4c0c2ac8813c648dbcb
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2025-05-19 22:42:41 -0600

    add gradcam

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import torchvision
 from PIL import Image
@@ -8,6 +9,8 @@ from torchvision.transforms import Compose
 import json
 from typing import Dict
 from torchvision.transforms import Lambda
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 
 app = Flask(__name__)
 
@@ -52,6 +55,17 @@ def predict():
         std=DS_STATS['std']
         )
     pred = model(tensor)
+
+    generate_gradcam(
+        model=model, 
+        pred_class=pred.argmax(dim=1).item(),
+        save_dir=Path('../data/output_imgs/'),
+        model_name=MODEL_PATH.rpartition("/")[2],
+        input_img=Image.open(Path(UPLOAD_PATH).joinpath(img_filename)),
+        input_tensor=tensor,
+        input_filename=img_filename
+        )
+
     return jsonify({'prediction': CLASS_LIST[pred.argmax(dim=1).item()]})
 
 
@@ -64,6 +78,33 @@ def prepare_img(filepath, mean, std):
     loaded_img = Image.open(filepath)
     norm_tensor = transforms(loaded_img).to(DEVICE)
     return norm_tensor
-    
-    
 
+
+def generate_gradcam(
+        model, 
+        pred_class, 
+        save_dir, 
+        model_name, 
+        input_img, 
+        input_tensor,
+        input_filename
+        ):
+    model.eval()
+    for param in model.parameters():
+        param.requires_grad = True
+    target_layers = [model.features[-4][0]]
+    targets = [ClassifierOutputTarget(pred_class)]
+
+    with GradCAM(model=model, target_layers=target_layers) as cam:
+        grayscale_cam = cam(input_tensor, targets=targets)
+
+    plt.imshow(input_img)
+    plt.imshow(grayscale_cam[0], alpha=0.5, cmap="turbo")
+    plt.tight_layout()
+    plt.axis('off')
+    plt.savefig(
+        save_dir.joinpath(f"{model_name}-{input_filename}-{pred_class}-gradcam.jpg"),
+        bbox_inches='tight',
+        pad_inches=0
+        )
+    plt.close()
